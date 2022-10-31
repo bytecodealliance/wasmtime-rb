c399bb976c151d672e8f1862f57a8d3f163497ac
Ian Ker-Seymer <ian.kerseymer@shopify.com>
2022-10-31 13:37:06 -0400

    Try to fix for windows

--- spec/unit/module_spec.rb
+++ spec/unit/module_spec.rb
@@ -1,5 +1,6 @@
 require "spec_helper"
 require "tempfile"
+require "securerandom"
 
 module Wasmtime
   RSpec.describe Module do
@@ -14,34 +15,32 @@ module Wasmtime
 
     describe(".deserialize_file") do
       it("can deserialize a module from a file") do
-        tmpfile = Tempfile.new(["deserialize-file", ".so"])
-        tmpfile.write(Module.new(engine, "(module)").serialize)
-        tmpfile.close
+        tmpfile = create_tmpfile(Module.new(engine, "(module)").serialize)
+        mod = Module.deserialize_file(engine, tmpfile)
 
-        begin
-          mod = Module.deserialize_file(engine, tmpfile.path)
-          expect(mod.serialize).to eq(Module.new(engine, "(module)").serialize)
-        ensure
-          tmpfile.unlink
-        end
+        expect(mod.serialize).to eq(Module.new(engine, "(module)").serialize)
       end
 
       it("deserialize from a module multiple times") do
-        tmpfile = Tempfile.new(["deserialize-file", ".so"])
-        tmpfile.write(Module.new(engine, wat).serialize)
-        tmpfile.close
+        tmpfile = create_tmpfile(Module.new(engine, wat).serialize)
 
-        begin
-          mod_one = Module.deserialize_file(engine, tmpfile.path)
-          mod_two = Module.deserialize_file(engine, tmpfile.path)
-          expected = Module.new(engine, wat).serialize
+        mod_one = Module.deserialize_file(engine, tmpfile)
+        mod_two = Module.deserialize_file(engine, tmpfile)
+        expected = Module.new(engine, wat).serialize
 
-          expect(mod_one.serialize).to eq(expected)
-          expect(mod_two.serialize).to eq(expected)
-        ensure
-          tmpfile.unlink
-        end
+        expect(mod_one.serialize).to eq(expected)
+        expect(mod_two.serialize).to eq(expected)
       end
+
+      def create_tmpfile(content)
+        uuid = SecureRandom.uuid
+        path = File.join(tmpdir, uuid)
+        File.write(path, content)
+        path
+      end
+
+      let(:tmpdir) { Dir.mktmpdir }
+      after(:each) { FileUtils.rm_rf(tmpdir) }
     end
 
     describe(".deserialize") do

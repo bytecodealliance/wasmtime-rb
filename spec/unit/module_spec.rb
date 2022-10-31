2a02f22e67ced6145c3a589df8c2fc0e45284981
Ian Ker-Seymer <ian.kerseymer@shopify.com>
2022-10-31 11:12:35 -0400

    Add Module.deserialize_file

--- spec/unit/module_spec.rb
+++ spec/unit/module_spec.rb
@@ -1,14 +1,47 @@
 require "spec_helper"
+require "tempfile"
 
 module Wasmtime
   RSpec.describe Module do
     let(:engine) { Engine.new(Wasmtime::Config.new) }
 
     it("can be serialized and deserialized") do
-      mod = Module.new(engine, "(module)")
+      mod = Module.new(engine, wat)
       serialized = mod.serialize
       deserialized = Module.deserialize(engine, serialized)
       expect(deserialized.serialize).to eq(serialized)
+    end
+
+    describe(".deserialize_file") do
+      it("can deserialize a module from a file") do
+        tmpfile = Tempfile.new(["deserialize-file", ".so"])
+        tmpfile.write(Module.new(engine, "(module)").serialize)
+        tmpfile.flush
+
+        begin
+          mod = Module.deserialize_file(engine, tmpfile.path)
+          expect(mod.serialize).to eq(Module.new(engine, "(module)").serialize)
+        ensure
+          tmpfile.close
+        end
+      end
+
+      it("deserialize from a module multiple times") do
+        tmpfile = Tempfile.new(["deserialize-file", ".so"])
+        tmpfile.write(Module.new(engine, wat).serialize)
+        tmpfile.flush
+
+        begin
+          mod_one = Module.deserialize_file(engine, tmpfile.path)
+          mod_two = Module.deserialize_file(engine, tmpfile.path)
+          expected = Module.new(engine, wat).serialize
+
+          expect(mod_one.serialize).to eq(expected)
+          expect(mod_two.serialize).to eq(expected)
+        ensure
+          tmpfile.close
+        end
+      end
     end
 
     describe(".deserialize") do

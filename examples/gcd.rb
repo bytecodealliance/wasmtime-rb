b9d7090d63c23e1ac69947b404f4903636c6d0c7
Jimmy Bourassa <jimmy.bourassa@shopify.com>
2022-11-18 17:15:22 -0500

    Add examples

---
+++ examples/gcd.rb
@@ -0,0 +1,8 @@
+require "wasmtime"
+
+engine = Wasmtime::Engine.new
+mod = Wasmtime::Module.from_file(engine, "examples/gcd.wat")
+store = Wasmtime::Store.new(engine)
+instance = Wasmtime::Instance.new(store, mod)
+
+puts "gcd(6, 27) = #{instance.invoke("gcd", 3, 1)}"

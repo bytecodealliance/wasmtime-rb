d903560cd4aa56dc2681f249a48736bc5dca69f9
Ian Ker-Seymer <ian.kerseymer@shopify.com>
2022-12-05 12:05:08 -0500

    Add rake task to run examples (#58)
    * Add rake task to run examples
    
    * Avoid ruby2 keywords in hello example
    
    * Make hello example 2.7 friendly and more explicit

--- examples/hello.rb
+++ examples/hello.rb
@@ -1,14 +1,27 @@
 require "wasmtime"
 
+class MyData
+  attr_reader :count
+
+  def initialize
+    @count = 0
+  end
+
+  def increment!
+    @count += 1
+  end
+end
+
+data = MyData.new
 engine = Wasmtime::Engine.new
 mod = Wasmtime::Module.from_file(engine, "examples/hello.wat")
-store = Wasmtime::Store.new(engine, {count: 0})
+store = Wasmtime::Store.new(engine, data)
 func = Wasmtime::Func.new(store, Wasmtime::FuncType.new([], [])) do |caller|
   puts "Hello from Func!"
-  caller.store_data[:count] += 1
+  caller.store_data.increment!
 end
 
 instance = Wasmtime::Instance.new(store, mod, [func])
 instance.invoke("run")
 
-puts "Store's count: #{store.data[:count]}"
+puts "Store's count: #{store.data.count}"

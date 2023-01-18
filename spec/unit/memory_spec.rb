8b0e2a00d789b8370fad276e6652deffbc04bb17
Ian Ker-Seymer <hello@ianks.com>
2023-01-18 12:52:20 -0500

    Do not test Slice#to_s on Ruby 3.0

--- spec/unit/memory_spec.rb
+++ spec/unit/memory_spec.rb
@@ -99,7 +99,7 @@ module Wasmtime
         expect(str).to be_frozen
       end
 
-      if RUBY_VERSION >= "3.0.0"
+      if defined?(Fiddle::MemoryView)
         it "exposes a memory view" do
           mem = Memory.new(store, min_size: 3)
           mem.write(0, "foo")
@@ -108,7 +108,7 @@ module Wasmtime
           expect(view).to be_a(Fiddle::MemoryView)
           expect(view).to be_readonly
           expect(view.ndim).to eq(1)
-          expect(view.to_s).to eq("foo")
+          expect(view.to_s).to eq("foo") unless RUBY_VERSION.start_with?("3.0")
         end
       end
 
@@ -121,7 +121,7 @@ module Wasmtime
         expect { slice.to_str }
           .to raise_error(Wasmtime::Error, "memory slice was invalidated by resize")
 
-        if RUBY_VERSION >= "3.0.0"
+        if defined?(Fiddle::MemoryView)
           expect { slice.to_memory_view }
             .to raise_error(ArgumentError, /Unable to get a memory view from/)
         end

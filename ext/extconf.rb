a0c61a0fc20e24d64b0a4aa628f920f671701d0f
Ian Ker-Seymer <ian.kerseymer@shopify.com>
2023-01-31 15:59:39 -0500

    Remove extconf hack

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -4,7 +4,3 @@ require "mkmf"
 require "rb_sys/mkmf"
 
 create_rust_makefile("wasmtime/wasmtime_rb")
-
-contents = File.read("Makefile")
-contents.gsub!("$(CARGO) rustc", "$(CARGO) rustc --crate-type cdylib")
-File.write("Makefile", contents)

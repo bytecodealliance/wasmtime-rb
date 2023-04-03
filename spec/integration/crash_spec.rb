52b7822b9e504a26366000d912bc03a9fba8e49d
Ian Ker-Seymer <ian.kerseymer@shopify.com>
2023-04-02 22:13:34 -0400

    Hone in on valgrind issue

--- spec/integration/crash_spec.rb
+++ spec/integration/crash_spec.rb
@@ -16,14 +16,14 @@ module Wasmtime
       end
     end
 
-    # it "ensures user exceptions are never GC'd" do
-    #   store = Store.new(engine, Object.new)
-    #   call_times = 0
-    #   func = Func.new(store, [], [:i32, :i32]) do
-    #     call_times += 1
-    #     # most GC-able exception ever?
-    #     raise Class.new(StandardError).new((+"hello") + SecureRandom.hex(6))
-    #   end
+    it "ensures user exceptions are never GC'd" do
+      store = Store.new(engine, Object.new)
+      call_times = 0
+      func = Func.new(store, [], [:i32, :i32]) do
+        call_times += 1
+        # most GC-able exception ever?
+        raise Class.new(StandardError).new((+"hello") + SecureRandom.hex(6))
+      end
 
     #   n_times.times do |i|
     #     expect { func.call }.to raise_error(StandardError, /^hello\h{12}$/)

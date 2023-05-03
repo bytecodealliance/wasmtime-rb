146b6f9d8450bf560f2e563325cb2d66594ce349
Ian Ker-Seymer <ian.kerseymer@shopify.com>
2023-05-02 21:32:12 -0400

    Track all Rust allocations made with `std::alloc::GlobalAlloc`

--- ext/src/lib.rs
+++ ext/src/lib.rs
@@ -8,6 +8,9 @@ pub use ruby_api::*;
 #[cfg(not(feature = "ruby-api"))]
 pub(crate) use ruby_api::*;
 
+#[cfg(all(feature = "global-tracking-allocator", not(feature = "ruby-api")))]
+rb_sys::set_global_tracking_allocator!();
+
 #[magnus::init]
 pub fn init() -> Result<(), Error> {
     #[cfg(ruby_gte_3_0)]

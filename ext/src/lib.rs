d746441d0a9772f6f15559745bbe25029aebcd05
Ian Ker-Seymer <ian.kerseymer@shopify.com>
2023-08-11 11:55:20 -0400

    Update deps

--- ext/src/lib.rs
+++ ext/src/lib.rs
@@ -8,7 +8,7 @@ pub use ruby_api::*;
 #[cfg(not(feature = "ruby-api"))]
 pub(crate) use ruby_api::*;
 
-#[cfg(all(not(feature = "ruby-api")))] // Let the upstream crate handle this
+#[cfg(not(feature = "ruby-api"))] // Let the upstream crate handle this
 rb_sys::set_global_tracking_allocator!();
 
 #[magnus::init]
